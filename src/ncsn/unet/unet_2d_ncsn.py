329022ae6a1dfffd2a3910e9124ef1eb22ec6285
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-12-29 18:35:25 +0900

    update to add notes for unet model (#11)

--- src/ncsn/unet/unet_2d_ncsn.py
+++ src/ncsn/unet/unet_2d_ncsn.py
@@ -76,7 +76,8 @@ class UNet2DModelForNCSN(UNet2DModel, ModelMixin, ConfigMixin):  # type: ignore[
             add_attention=add_attention,
             class_embed_type=class_embed_type,
             num_class_embeds=num_class_embeds,
-            num_train_timesteps=num_train_timesteps,
+            # TODO: If you specify num_train_timesteps here, the quality of the generated product will suddenly decrease ;(
+            # num_train_timesteps=num_train_timesteps,
         )
         sigmas = torch.exp(
             torch.linspace(
